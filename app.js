d1631a91811b77ca2a481ae9215814fa0ff0d63a
Kate Schlunz <schlunzk@gmail.com>
2017-11-19 18:42:31 +0000

    automatically backed up by learn

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 var express = require('express')
 var app = express()
 
-// Build Your Route Here
+app.get("/", function(req, resp){
+  const welcomeText= "WELCOME TO MY PAGE"
+  resp.render('welcomeText')
 
+})
 
 module.exports = app
